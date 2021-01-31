09168c74a605f406794ddcab72fac2c45336dbaf
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2021-01-31 13:26:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 version = "0.5.4"
 
@@ -7,7 +7,6 @@ setup(
     author="ccev",
     url="https://github.com/ccev/pogodata",
     version=version,
-    packages=find_packages(),
     install_requires=["requests"],
     packages=["pogodata"],
     long_description="For documentation, please visit https://github.com/ccev/pogodata",
