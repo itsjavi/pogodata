378723e258d9e7e25b538c5f8695b01d1fc30d74
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2021-01-31 12:52:17 +0100

    add find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = "0.5.4"
 
@@ -7,8 +7,9 @@ setup(
     author="ccev",
     url="https://github.com/ccev/pogodata",
     version=version,
+    packages=find_packages(),
     install_requires=["requests"],
     packages=["pogodata"],
-    long_description="For documentation, plase visit https://github.com/ccev/pogodata",
+    long_description="For documentation, please visit https://github.com/ccev/pogodata",
     description="Easy and up-to-date Pogo Data"
 )
